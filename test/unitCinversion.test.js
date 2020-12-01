ce042b11d182510a89f450f2068c3aded3e51bc6
JONNAGADDALA RAVIKUMAR <ravikumarjit583@gmail.com>
2020-12-01 15:02:13 +0530

    UC1_TC1.2: Comparing 0 Feet and o Feet

--- test/unitCinversion.test.js
+++ test/unitCinversion.test.js
@@ -1,9 +1,16 @@
 const QuantityConverter = require('../main/UnitConverterAndCalculater');
 const Units = require('../main/UnitObjects')
-
-test('TestCasePassedIfTheReturnedValueIsTrue_WhenWeassed_OneFeet,12Inches', () => {
-    const length = new Units();
-    const firstValue = new QuantityConverter(length.unit.FEET, 1);
-    const secondValue = new QuantityConverter(length.unit.INCH, 12);
-    expect(firstValue.compare(secondValue)).resolves.toBeTruthy();
-});
+describe('UseCase 1', () => {
+    test('TestCasePassedIfTheReturnedValueIsTrue_WhenWeassed_OneFeet,12Inches', () => {
+        const length = new Units();
+        const firstValue = new QuantityConverter(length.unit.FEET, 1);
+        const secondValue = new QuantityConverter(length.unit.INCH, 12);
+        expect(firstValue.compare(secondValue)).resolves.toBeTruthy();
+    });
+    test('TestCasePassedIfTheReturnedValueIsTrue_WhenWeassed_OneFeet,12Inches', () => {
+        const length = new Units();
+        const firstValue = new QuantityConverter(length.unit.FEET, 0);
+        const secondValue = new QuantityConverter(length.unit.FEET, 0);
+        expect(firstValue.compare(secondValue)).resolves.toBeTruthy();
+    });
+})
